f48150c68feead8c771f5ea0b5282d87cd979066
Petar Koljensic <104145970+PeterKammFKV@users.noreply.github.com>
2024-04-01 19:02:15 +0200

    Update 1_top_paying.sql
    2

--- sql_files/1_top_paying.sql
+++ sql_files/1_top_paying.sql
@@ -1,5 +1,6 @@
-SELECT job_title_short, job_location, salary_year_avg, job_id
+SELECT job_title_short, job_location, salary_year_avg, job_id, name AS company_name
 FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
 	salary_year_avg IS NOT Null AND job_title_short = 'Data Analyst' AND job_location = 'Anywhere'
 ORDER BY
